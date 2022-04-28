3d2309319a17468cc37c1b69a361cc9de76739bd
Bunnie <bunniecodes@gmail.com>
2022-04-27 23:55:40 -0700

    commit

--- tictactoePVP/tictactoePVP.py
+++ tictactoePVP/tictactoePVP.py
@@ -1,10 +1,27 @@
-from tttPVP_layout import GameLayout
-
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
 
-class TicTacToePVP(App):
+class GameFunction(BoxLayout):
+    space_1 = StringProperty("1")
+    space_2 = StringProperty("2")
+    space_3 = StringProperty("3")
+    space_4 = StringProperty("4")
+    space_5 = StringProperty("5")
+    space_6 = StringProperty("6")
+    space_7 = StringProperty("7")
+    space_8 = StringProperty("8")
+    space_9 = StringProperty("9")
+    pass
+
+class GameLayout(BoxLayout):
+    pass
+
+class TicTacToe(App):
     def build(self):
         return GameLayout()
 
 if __name__ == "__main__":
-    TicTacToePVP().run()
+    TicTacToe().run()
