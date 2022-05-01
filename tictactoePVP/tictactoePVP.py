d0bccbfe57aad1785203fc667cf959f856ab5abf
Bunnie <bunniecodes@gmail.com>
2022-05-01 00:04:24 -0700

    commit

--- tictactoePVP/tictactoePVP.py
+++ tictactoePVP/tictactoePVP.py
@@ -1,27 +1,60 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 
-class GameFunction(BoxLayout):
-    space_1 = StringProperty("1")
-    space_2 = StringProperty("2")
-    space_3 = StringProperty("3")
-    space_4 = StringProperty("4")
-    space_5 = StringProperty("5")
-    space_6 = StringProperty("6")
-    space_7 = StringProperty("7")
-    space_8 = StringProperty("8")
-    space_9 = StringProperty("9")
-    pass
+class GameMethods(BoxLayout):
+    buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    player = "X"
+    winner = False
+    text = StringProperty("Tic Tac Toe!")
+    bEnabled = True
+    theWins = [
+        # horizantal win conditions
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
 
-class GameLayout(BoxLayout):
-    pass
+        # vertical win conditions
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        
+        # diagonal win conditions
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    # gives the parameters of a turn
+    def onPress(self, widget):
+        # places an X in the space during X's turn
+        if self.player == "X":
+            self.wins()
+            x = int(widget.text) - 1
+            widget.text = self.player
+            self.buttons[x] = self.player
+            self.wins()
+            self.player = "O"
+            widget.disabled = True
+
+        # places an O in the space if it is a valid input when it is O's turn
+        else:
+            x = int(widget.text) - 1
+            widget.text = self.player
+            self.buttons[x] = self.player
+            self.wins()
+            self.player = "X"
+            widget.disabled = True
+
+    # defines how to win the game and checks if met
+    def wins(self):
+        if any(self.buttons[win[0]] == self.buttons[win[1]] == self.buttons[win[2]] for win in self.theWins):
+            self.text = f"Congrats {self.player} has won!"
+            self.winner = True
 
 class TicTacToe(App):
     def build(self):
-        return GameLayout()
+        return GameMethods()
 
 if __name__ == "__main__":
     TicTacToe().run()
