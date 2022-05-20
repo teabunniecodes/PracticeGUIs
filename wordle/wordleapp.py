28fbf8378f803b081e9e38074d4620fada201c04
Bunnie <bunniecodes@gmail.com>
2022-05-19 18:17:29 -0700

    Wordle GUI layout is updated to a base on how it should look

--- wordle/wordleapp.py
+++ wordle/wordleapp.py
@@ -2,14 +2,19 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 
 class WordleLayout(BoxLayout):
     pass
 
+class WordleMethods():
+    turns = 6
+
 class WordleApp(App):
-    def build(self):
-        return WordleLayout()
+    # def build(self):
+    #     return WordleLayout()
+    pass
 
 if __name__ == "__main__":
     WordleApp().run()
